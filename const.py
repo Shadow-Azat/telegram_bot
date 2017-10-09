4b63aaa847c4a4b484a83b1c46d0fb551c794e4f
Shadow-Azat <nagimov.azat@gmail.com>
2017-10-09 22:08:49 +0300

    first exp

---
+++ const.py
@@ -0,0 +1,3 @@
+token = "296701087:AAGeT3v4xufoP9qW2W1vX884HEomo7AWKpw"
+my_id = 163794402
+id_list = [123, 223]
---
+++ main.py
@@ -0,0 +1,63 @@
+# # telegram bot for Barrel team
+#
+# """
+#
+# The first edition
+#
+# """
+#
+# import telebot
+import const
+#
+# bot = telebot.TeleBot(const.token)
+#
+# #bot.send_message(const.my_id, "test")
+#
+# upd = bot.get_updates()
+#
+# #print(upd)
+#
+# last_upd = upd[-1]
+#
+# message_from_user = last_upd.message
+#
+# print(message_from_user)
+#
+# print(bot.get_me())
+#
+# def log(message, answer):
+#     print("\n --------------------")
+#     from datetime import datetime
+#     print(datetime.now())
+#     print("Сообщение от {0} {1} (id = {2}) \n Текст: {3}".format(message.from_user.first_name,
+#                                                                   message.from_user.last_name,
+#                                                                   str(message.from_user.id),
+#                                                                   message.text))
+#     print(answer)
+#
+#
+# @bot.message_handler(commands = ['help'])
+# def handle_text(message):
+#         bot.send_message(message.chat.id, """вроде все работает""")
+#
+# @bot.message_handler(content_types = ['text'])
+# def handle_text(message):
+#
+#     answer = "неизвестная команда"
+#     if message.text == "test":
+#         answer = "вроде работает"
+#         log(message,answer)
+#         bot.send_message(message.chat.id, answer)
+#
+#     elif message.text == "привет" or message.text == "Привет":
+#         answer = "привет, "
+#         log(message, answer)
+#         bot.send_message(message.chat.id, answer + message.from_user.first_name)
+#     else:
+#    #    bot.send_message(message.chat.id, answer)
+#         log(message,answer)
+#
+#
+#
+# bot.polling(none_stop=True, interval=0)
+print(const.id_list[1])
